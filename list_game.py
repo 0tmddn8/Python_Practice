3f05eec6d23652c6cc5130b6e5c981be3bc29173
seungwoo08 <ssounsowns@gmail.com>
2023-01-14 02:02:30 +0900

    list_game.py
    list_game

---
+++ list_game.py
@@ -0,0 +1,24 @@
+import random
+list_game = ["바위", "가위", "보"]
+rand_game = random.choice(list_game)
+print(rand_game)
+game = input('game:')
+print(game)
+if game == "바위" and rand_game == "가위":
+    print("이김")
+if game == "바위" and rand_game == "보":
+    print("짐")
+if game == "바위" and rand_game == "바위":
+    print("비김")
+if game == "보" and rand_game == "가위":
+    print("짐")
+if game == "보" and rand_game == "보":
+    print("비김")
+if game == "보" and rand_game == "바위":
+    print("이김")
+if game == "가위" and rand_game == "가위":
+    print("비김")
+if game == "가위" and rand_game == "보":
+    print("이김")
+if game == "가위" and rand_game == "바위":
+    print("짐")
